f6fff309dcbba4532e0b9361d591f48a8330eaf9
kashyappanda <kashyap.panda@gmail.com>
2016-08-28 23:54:35 -0700

    Add logo animation

--- js/blocs.js
+++ js/blocs.js
@@ -221,24 +221,46 @@ function setUpLightBox()
 	{
 		e.preventDefault();
 		targetLightbox = $(this);
-		var captionData ='<p class="lightbox-caption">'+$(this).attr('data-caption')+'</p>';
-		if(!$(this).attr('data-caption')) // No caption caption data
+		var captionData ='<p class="lightbox-caption">'+targetLightbox.attr('data-caption')+'</p>';
+		var galleryID = 'no-gallery-set';
+		
+		if(targetLightbox.attr('data-gallery-id')) // Has a gallery ID so use it
+		{
+			galleryID = targetLightbox.attr('data-gallery-id');
+		}
+		
+		if(!targetLightbox.attr('data-caption')) // No caption caption data
 		{
 			captionData = '';
 		}
 		
-		var customModal = $('<div id="lightbox-modal" class="modal fade"><div class="modal-dialog"><div class="modal-content '+$(this).attr('data-frame')+'"><button type="button" class="close close-lightbox" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><div class="modal-body"><a href="#" class="prev-lightbox" aria-label="prev"></a><a href="#" class="next-lightbox" aria-label="next"></a><img id="lightbox-image" class="img-responsive" src="'+$(this).attr('data-lightbox')+'">'+captionData+'</div></div></div></div>');
+		var customModal = $('<div id="lightbox-modal" class="modal fade"><div class="modal-dialog"><div class="modal-content '+targetLightbox.attr('data-frame')+'"><button type="button" class="close close-lightbox" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><div class="modal-body"><a href="#" class="prev-lightbox" aria-label="prev"></a><a href="#" class="next-lightbox" aria-label="next"></a><img id="lightbox-image" class="img-responsive" src="'+targetLightbox.attr('data-lightbox')+'">'+captionData+'</div></div></div></div>');
 		$('body').append(customModal);
 		$('#lightbox-modal').modal('show');
 		
-		// Handle navigation buttons (next - prev)
-		if($('a[data-lightbox]').index(targetLightbox) == 0)
+		if(galleryID == 'no-gallery-set') // No Gallery ID
 		{
-			$('.prev-lightbox').hide();
+			// Handle navigation buttons (next - prev)
+			if($('a[data-lightbox]').index(targetLightbox) == 0)
+			{
+				$('.prev-lightbox').hide();
+			}
+			if($('a[data-lightbox]').index(targetLightbox) == $('a[data-lightbox]').length-1)
+			{
+				$('.next-lightbox').hide();
+			}
 		}
-		if($('a[data-lightbox]').index(targetLightbox) == $('a[data-lightbox]').length-1)
+		else // Has Gallery ID
 		{
-			$('.next-lightbox').hide();
+			// Handle navigation buttons (next - prev)
+			if($('a[data-gallery-id="'+galleryID+'"]').index(targetLightbox) == 0)
+			{
+				$('.prev-lightbox').hide();
+			}
+			if($('a[data-gallery-id="'+galleryID+'"]').index(targetLightbox) == $('a[data-gallery-id="'+galleryID+'"]').length-1)
+			{
+				$('.next-lightbox').hide();
+			}
 		}
 	}
 	).on('hidden.bs.modal', '#lightbox-modal', function () // Handle destroy modal 
@@ -249,13 +271,27 @@ function setUpLightBox()
 	$(document).on('click', '.next-lightbox, .prev-lightbox', function(e) 
 	{
 		e.preventDefault();
+		var galleryID = 'no-gallery-set';
 		var idx = $('a[data-lightbox]').index(targetLightbox);
 		var next = $('a[data-lightbox]').eq(idx+1) // Next
 		
+		if(targetLightbox.attr('data-gallery-id')) // Has Gallery ID so Use
+		{
+			galleryID = targetLightbox.attr('data-gallery-id'); // ID
+			idx = $('a[data-gallery-id="'+galleryID+'"]').index(targetLightbox); // Index
+			next = $('a[data-gallery-id="'+galleryID+'"]').eq(idx+1) // Next
+		}
+		
 		if($(this).hasClass('prev-lightbox'))
 		{
-			next = $('a[data-lightbox]').eq(idx-1) // Prev
+			next = $('a[data-gallery-id="'+galleryID+'"]').eq(idx-1) // Prev
+			
+			if(galleryID == 'no-gallery-set') // No Gallery ID
+			{
+				next = $('a[data-lightbox]').eq(idx-1) // Prev
+			}
 		}
+		
 		$('#lightbox-image').attr('src',next.attr('data-lightbox'));
 		$('.lightbox-caption').html(next.attr('data-caption'));
 		targetLightbox = next;	
@@ -263,13 +299,27 @@ function setUpLightBox()
 		// Handle navigation buttons (next - prev)
 		$('.next-lightbox, .prev-lightbox').hide();	
 		
-		if($('a[data-lightbox]').index(next) != $('a[data-lightbox]').length-1)
+		if(galleryID == 'no-gallery-set') // No Gallery ID
 		{
-			$('.next-lightbox').show();
+			if($('a[data-lightbox]').index(next) != $('a[data-lightbox]').length-1)
+			{
+				$('.next-lightbox').show();
+			}
+			if($('a[data-lightbox]').index(next) > 0)
+			{
+				$('.prev-lightbox').show();
+			}
 		}
-		if($('a[data-lightbox]').index(next) > 0)
+		else // Has Gallery ID
 		{
-			$('.prev-lightbox').show();
+			if($('a[data-gallery-id="'+galleryID+'"]').index(next) != $('a[data-gallery-id="'+galleryID+'"]').length-1)
+			{
+				$('.next-lightbox').show();
+			}
+			if($('a[data-gallery-id="'+galleryID+'"]').index(next) > 0)
+			{
+				$('.prev-lightbox').show();
+			}
 		}
 	});
 }
