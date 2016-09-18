f4a1d0f86932172380d0d99cc383674e7e148bb4
Sriharsha Guduguntla <sguduguntla11@gmail.com>
2016-09-17 22:28:35 -0700

    Adding menu

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
 
+  //Smooth Scrolling
+  
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
   orderClouds(6, 11, "multipleClouds", 3000);
 
   function orderClouds(min, max, id, interval) {
@@ -11,6 +26,44 @@ $(document).ready(function() {
       if (cloudNum == max) cloudNum = min;
       cloudType = "clouds" + cloudNum;
     }, interval);
+  }
+
+  var sidebarIsOpen = false;
+
+  $("#menuBtn").click(function(e) {
+    e.preventDefault();
+    if (sidebarIsOpen == false) {
+      sidebarIsOpen = true;
+    } else {
+      sidebarIsOpen = false;
+    }
+
+    controlSidebar();
+
+    $(this).toggleClass("active");
+
+  });
+
+  $("#sidebarCircle").click(function(e) {
+    e.preventDefault();
+    sidebarIsOpen = false;
+    controlSidebar();
+    $(".nav-toggle").removeClass("active");
+  });
+
+  function controlSidebar() {
+    if (!sidebarIsOpen) {
+      //Close
+      $(".sidebarContainer").css("right", "-50%");
+      $(".menuContainer").css("background", "black");
+
+    } else {
+      //Open
+      $(".sidebarContainer").css("right", "0%");
+      $(".menuContainer").css("background", "transparent");
+    }
+
+    $(".sidebarContainer").css("transition", "right 1s, background 3s ease");
   }
 
 });
