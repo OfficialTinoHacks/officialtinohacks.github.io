2083128a6655945530ce128c9cc27c055716858e
Sriharsha Guduguntla <sguduguntla11@gmail.com>
2017-04-13 01:13:56 -0700

    tinohacks live website complete

--- js/live.js
+++ js/live.js
@@ -14,46 +14,6 @@ $(document).ready(function() {
         }
     });
 
-    addJudgeInfo(function() {
-        $(".teamPhoto").hover(function() {
-            $(this).css("cursor", "pointer");
-            $(this).css("margin-top", "-10px");
-            $(this).css("transition", "margin-top 0.3s ease");
-        }, function() {
-            $(this).css("cursor", "auto");
-            $(this).css("margin-top", "0px");
-            $(this).css("transition", "margin-top 0.3s ease");
-        });
-    });
-
-    function addJudgeInfo(callback) {
-
-        getJudgesJSON(function(data) {
-            for (var i = 0; i < data.length; i++) {
-                var teamHTML = '<h2 class="text-center">';
-                teamHTML += '<div class="center-block circle teamPhoto"><div style="display: none;" class="circle photo-overlay"><p>Learn more</p></div></div><br/>' + data[i].name + '</h2>'
-                teamHTML += '<p class="text-center teamPos">'
-                teamHTML += '<p class="teamPos"><span style="color: #BDBEC0">' + data[i].title + '</span><br/>' + data[i].company + '</p>';
-                teamHTML += '</p>';
-
-                $("#judge" + (i + 1)).html(teamHTML);
-
-                $("#judge" + (i + 1) + " > h2 > div.teamPhoto").css("background-image", "url('" + data[i].img + "')");
-
-                teamHTML = "";
-
-                callback();
-            }
-        });
-
-    }
-
-    function getJudgesJSON(callback) {
-        //Load team info from json file
-        $.getJSON("resources/judgeinfo.json", function(data) {
-            return callback(data);
-        });
-    }
 
 
 });
