8d55571715d60a1e8766ab2fdcbc7258e81b4e44
Sriharsha Guduguntla <sguduguntla11@gmail.com>
2017-09-14 18:56:22 -0700

    Updating entire FAQ for tinohacks

--- 2018/js/main.js
+++ 2018/js/main.js
@@ -27,33 +27,49 @@ $(document).ready(function() {
             {
                 backgroundImage: "url(img/logo-small.png)",
                 title: "What is TinoHacks?",
-                text: "TinoHacks II is a two day long, 350 person hackathon hosted by Cupertino High School students where both new and experienced programmers can work together on projects, learn new skills, and have a splendid time. TinoHacks I happened on April 15-16, 2017, and TinoHacks is coming back in 2018 with our second event! It is backed by Hack +, a 501 (c)(3)public charity registered in CA.All donations are tax - deductible."
+                text: "TinoHacks II is a two day long, 350 person hackathon hosted by Cupertino High School students where both new and experienced programmers can work together on projects, learn new skills, and have a splendid time. TinoHacks I happened on April 15-16, 2017, and TinoHacks is coming back in 2018 with our second event! It is backed by Hack +, a 501 (c)(3)public charity registered in CA. All donations are tax - deductible."
             }, {
-                backgroundImage: "url(img/bgimages/title-logo.png)",
+                backgroundImage: "url(img/faq/tinohacks1.jpg)",
                 title: "What is a hackathon?",
                 text: "A Hackathon is a 24 to 36 hour long coding marathon event where people can make the most bizarre ideas into reality. Over the course of the weekend, students collaborate to create projects, listen to exquisite workshop presentations, exercise their programming abilities, and of course, have tons of fun!"
             }, {
-                backgroundImage: "url(img/logo-small.png)",
-                title: "Cost?",
-                text: "Thanks to our sponsors, this event is completely free! This includes all the food, swag, and fantabulous prizes."
+                backgroundImage: "url(img/faq/tinohacks2.jpg)",
+                title: "Time and Location?",
+                text: "Time and location are TBD. Thanks to our sponsors, this event is completely free! This includes all the food, swag, and fantabulous prizes."
             }, {
-                backgroundImage: "url(img/logo-small.png)",
-                title: "What will I need?",
-                text: "You will need a laptop, a valid student ID (or some sort of student ID), chargers, your phone, a blanket/sleeping bag, toiletries, and any supplies you need for your hack. All MLH Hardware will be available for rent at the event. All TinoHacks participants are guaranteed top notch iMacs with pre-installed programming software to hack throughout the entire event, however, you may bring your own laptop for convenience as well!"
-            }, {
-                backgroundImage: "url(img/logo-small.png)",
+                backgroundImage: "url(img/faq/tinohacks3.jpg)",
                 title: "Do I need programming experience?",
                 text: "No! In fact, TinoHacks is a great way to begin your programming experience! A basic understanding is recommended, but we will have workshops and mentors to help you make your project idea a reality."
             }, {
-                backgroundImage: "url(img/logo-small.png)",
-                title: "Who can attend?",
-                text: "This event is open to high schoolers only. However, college students may sign up to mentor, volunteer, or lead a workshop. Sign up here, or email <strong><a href='mailto:hello@tinohacks.tech'>hello@tinohacks.tech</a></strong> if you have any questions or concerns."
+                backgroundImage: "url(img/faq/tinohacks4.jpg)",
+                title: "What will I need?",
+                text: "You will need a laptop, a valid student ID (or some sort of student ID), chargers, your phone, a blanket/sleeping bag, toiletries, and any supplies you need for your hack. All MLH Hardware will be available for rent at the event."
             }, {
-                backgroundImage: "url(img/logo-small.png)",
+                backgroundImage: "url(img/faq/tinohacks5.jpg)",
+                title: "Who can attend?",
+                text: "This event is open to high schoolers only. However, college students may sign up to mentor, volunteer, or lead a workshop. Sign up here, or email <strong><a style='color: #377bb5 !important' href='mailto:hello@tinohacks.tech'>hello@tinohacks.tech</a></strong> if you have any questions or concerns."
+            }, {
+                backgroundImage: "url(img/faq/tinohacks6.jpg)",
                 title: "Do I need to stay overnight?",
                 text: "No; you will be permitted to go home at a certain time, though staying the night is highly recommended. The doors will close at 7pm and open at 7am, and no persons are allowed to leave or enter the building during that time."
+            }, {
+                backgroundImage: "url(img/faq/tinohacks7.jpg)",
+                title: "Team Requirements?",
+                text: "You need at least 1 person per team and 4 people at most."
+            },
+            {
+                backgroundImage: "url(img/faq/tinohacks8.jpg)",
+                title: "Travel Reimbursements?",
+                text: "Unfortunately, TinoHacks will not be offering travel reimbursements. Please email us at <strong><a style='color: #377bb5 !important' href='mailto:hello@tinohacks.tech'>hello@tinohacks.tech</a></strong> for additional details."
+            },
+            {
+                backgroundImage: "url(img/faq/tinohacks9.jpg)",
+                title: "Supervision and Code of Conduct?",
+                text: "There will be adult supervision at the event at all times. At night at 7pm, doors will close and lock until 7am the next morning. We will be following the <a target='_blank' style='color:#377bb5 !important' href='http://static.mlh.io/docs/mlh-code-of-conduct.pdf'>MLH code of conduct</a>, <a target='_blank' style='color:#377bb5 !important' href='https://github.com/MLH/policies/blob/master/data-sharing.md'>MLH Policies</a> and Hacker Fund Parent Packet."
             }
         ];
+
+        console.log(faqInfo);
 
         $("#faqImg").css("background-image", faqInfo[index].backgroundImage);
         $("#faqTitle").html(faqInfo[index].title);
