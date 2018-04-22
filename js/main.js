43212f163438344fdd49b3100c3f4c0a80526f58
Sriharsha Guduguntla <sguduguntla11@gmail.com>
2018-04-22 15:37:08 -0700

    Update site

--- js/main.js
+++ js/main.js
@@ -1,55 +1,5 @@
 $(document).ready(function () {
 
-    // Initialize Firebase
-    var config = {
-        apiKey: "AIzaSyDmBBGkNjjv1uylYjgJZKhUaQerUdxCw3w",
-        authDomain: "tinohacks2.firebaseapp.com",
-        databaseURL: "https://tinohacks2.firebaseio.com",
-        projectId: "tinohacks2",
-        storageBucket: "tinohacks2.appspot.com",
-        messagingSenderId: "779949253339"
-    };
-    firebase.initializeApp(config);
-
-    const db = firebase.database();
-
-    //Countdown timer to April 22, 2018, 10:00 AM
-    var x;
-    var countDownDate = new Date("Apr 22, 2018 10:00:00 AM").getTime();
-
-    db.ref('timerOn').on('value', (snapshot) => {
-        if (snapshot.val() == 0) {
-            clearInterval(x);
-            $("#countdown-timer").html("24 : 00 : 00");
-        } else {
-            // Update the count down every 1 second
-            x = setInterval(function () {
-
-                var now = new Date().getTime();
-
-                var distance = countDownDate - now;
-
-                var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-                var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) +
-                    (
-                        days * 24);
-                var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-                var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-                $("#countdown-timer").html(((hours < 10) ? "" + 0 + hours : hours) + " : " + ((
-                        minutes < 10) ? "" + 0 +
-                    minutes : minutes) + " : " + ((seconds < 10) ? "" + 0 + seconds :
-                    seconds));
-
-                if (distance < 0) {
-                    clearInterval(x);
-                    $("#hackingTimeLeft").html("<strong>END OF HACKING</strong>");
-                }
-            }, 1000);
-
-        }
-    });
-
     getTeamJSON(function (data) {
         for (var i = 0; i < data.length; i++) {
             var teamHTML = '';
